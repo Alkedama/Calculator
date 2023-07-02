07ba7eb95cc3420c1032df68879aafbfb606505a
King-Windows <krsazon@gmail.com>
2023-07-01 18:08:31 -0700

    Finished Operate

--- script.js
+++ script.js
@@ -1,88 +1,30 @@
 'use strict';
 
-let add = (a, b) => {
-    return a + b;
-}
-
-let subtract = (a, b) => {
-    return a - b;
-}
-
-let multiply = (a, b) => {
-    return a * b;
-}
-
-let divide = (a, b) => {
-    return a / b;
-}
-
 let num1 = [];
 let num2 = [];
 let operator;
-let processNum1;
-let processNum2;
+let answer = 0;
 
 const containerNumbers = document.querySelector('.containerNumbers');
 const buttonNumbers = containerNumbers.querySelectorAll('button');
 
 const operateButton = document.querySelector('#btnOperate');
 
-const pNum1 = document.createElement('span');
-const pNum2 = document.createElement('span');
-const pOperator = document.createElement('span');
-const pAnswer = document.createElement('span');
-
+const pNum = document.createElement('span');
 
 const operatorContainer = document.querySelector('.containerOperator');
 const operatorButton = operatorContainer.querySelectorAll('button');
 
-
-buttonNumbers.forEach(buttons => {
-    buttons.addEventListener('click', function (e) {
-        if (operator === undefined) {
-            num1.push(buttons.textContent);
-           
-            containerNumbers.appendChild(pNum1);
-
-            pNum1.textContent += buttons.textContent;
-
-            console.log('num1:' + num1);
-
-        } else if (operator !== undefined) {
-            num2.push(buttons.textContent);
-
-            containerNumbers.appendChild(pNum2);
-            pNum2.textContent += buttons.textContent;
-
-            console.log('num2:' + num2);
-        }
-
-    });
-
-});
-
-operatorButton.forEach(buttons => {
-    buttons.addEventListener('click', function (e) {
-
-        if (num1.length !== 0 && num2.length === 0) {
-            operator = buttons.textContent;
-
-            containerNumbers.appendChild(pOperator);
-
-            console.log(operator);
-            pOperator.textContent = " " + buttons.textContent + " ";
-        }
-
-    });
-});
+const btnOperate = document.querySelector('#btnOperate');
 
 function operate(a, b, operator) {
     let aProcess = a.join("");
     let bProcess = b.join("");
-    let answer;
+    
+    pNum.textContent = "";
+    num2.length = 0;
 
-    containerNumbers.appendChild(pAnswer);
-
+    if (answer === 0) {
     switch(operator){
     case '+':
     answer = parseInt(aProcess) + parseInt(bProcess);
@@ -97,17 +39,97 @@ function operate(a, b, operator) {
     answer = parseInt(aProcess) / parseInt(bProcess);
     break;
     default:
-    pAnswer.textContent = "Invalid Operator";
+    pNum.textContent = "Invalid Operator";
     }
-
-    pAnswer.textContent = " = " + answer;
+} else {
+    switch(operator){
+    case '+':
+    answer += parseInt(bProcess);
+    break;
+    case '-':
+    answer -= parseInt(bProcess);
+    break;
+    case '*':
+    answer *= parseInt(bProcess);
+    break;
+    case '/':
+    answer /= parseInt(bProcess);
+    break;
+    default:
+    pNum.textContent = "Invalid Operator";
+    }
 }
 
-const btnOperate = document.querySelector('#btnOperate');
+    console.log(answer);
+    pNum.textContent = " = " + answer;
+}
 
-btnOperate.addEventListener('click', function (e) {
+// Numbers
+buttonNumbers.forEach(buttons => {
+    buttons.addEventListener('click', function (e) {
+        console.log('num2 array.length: ' + num2.length);
+        if (operator === undefined && answer === 0) {
+            num1.push(buttons.textContent);
+           
+            containerNumbers.appendChild(pNum);
 
-    const c1 = new operate(num1, num2, operator);
+            pNum.textContent += buttons.textContent;
+
+            console.log('num1:' + num1);
+
+        } else if (operator !== undefined) {
+
+            if (num1.length !== 0 && num2.length === 0 || num1.length !== 0 && num2.length === 0 && answer !== 0) {
+            pNum.textContent = "";
+            }
+
+            num2.push(buttons.textContent);
+
+            containerNumbers.appendChild(pNum);
+            pNum.textContent += buttons.textContent;
+
+            console.log('num2:' + num2);
+        }
+        
+    });
 
 });
 
+// Operators
+operatorButton.forEach(buttons => {
+    buttons.addEventListener('click', function (e) {
+
+        if (num1.length !== 0 && num2.length === 0) {
+            operator = buttons.textContent;
+
+            console.log(operator);
+
+        } else if (num1.length !== 0 && num2.length !== 0) {
+            console.log('num2: ' + operator);
+            operator = buttons.textContent;
+
+            pNum.textContent = "";
+
+            new operate(num1, num2, operator);
+        }
+    });
+});
+
+// Equal
+btnOperate.addEventListener('click', function (e) {
+
+    new operate(num1, num2, operator);
+
+});
+
+// Clear
+const btnClear = document.querySelector('#btnClear');
+
+btnClear.addEventListener('click', function (e) {
+    num1.length = 0;
+    num2.length = 0;
+    operator = undefined;
+    answer = 0;
+
+    pNum.textContent = "";
+});
